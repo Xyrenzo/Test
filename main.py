5d276cedc911e19dd2887fc03b1bb4fa9a133e5a
Xyrenzo <166607781+Xyrenzo@users.noreply.github.com>
2025-08-30 22:08:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 posts = {
-    "en": {"title": "Hello World!", "body": "This is the first post.", "img": "/static/img/post.jpg"},
-    "ru": {"title": "Привет, мир!", "body": "Это первый пост.", "img": "/static/img/post.jpg"},
-    "ja": {"title": "こんにちは世界！", "body": "これは最初の投稿です。", "img": "/static/img/post.jpg"},
-    "ko": {"title": "안녕하세요 세계!", "body": "이것은 첫 번째 게시물입니다.", "img": "/static/img/post.jpg"},
-    "zh": {"title": "你好，世界！", "body": "这是第一篇帖子。", "img": "/static/img/post.jpg"},
-    "es": {"title": "¡Hola Mundo!", "body": "Esta es la primera publicación.", "img": "/static/img/post.jpg"},
-    "pt": {"title": "Olá Mundo!", "body": "Este é o primeiro post.", "img": "/static/img/post.jpg"},
+    "en": {"title": "Hello World!", "body": "This is the first post.", "img": "/img/4057491150f2b43a0810764ddb241818.jpg "},
+    "ru": {"title": "Привет, мир!", "body": "Это первый пост.", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
+    "ja": {"title": "こんにちは世界！", "body": "これは最初の投稿です。", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
+    "ko": {"title": "안녕하세요 세계!", "body": "이것은 첫 번째 게시물입니다.", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
+    "zh": {"title": "你好，世界！", "body": "这是第一篇帖子。", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
+    "es": {"title": "¡Hola Mundo!", "body": "Esta es la primera publicación.", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
+    "pt": {"title": "Olá Mundo!", "body": "Este é o primeiro post.", "img": "/img/4057491150f2b43a0810764ddb241818.jpg"},
 }
 
 # комменты храним как список словарей {text, lang}
